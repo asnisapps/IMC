f180658434a4cd1db82e9cd07906952ff12d937d
Logon Aluno <logonaluno@local.com>
2020-01-27 23:00:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/asnisapps/imc/extensions/EditTextExt.kt
@@ -0,0 +1,10 @@
+package br.com.asnisapps.imc.extensions
+
+import android.widget.EditText
+
+fun EditText.value() = this.text.toString()
+
+fun EditText.valueInt() = this.text.toString().toInt()
+
+fun EditText.valueDouble() = this.text.toString().toDouble()
+
